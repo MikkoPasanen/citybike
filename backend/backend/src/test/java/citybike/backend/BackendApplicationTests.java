211162dd7b8da6b130ccc998228465c0964af5be
Mikko Pasanen <pasanemikko@gmail.com>
2024-08-26 15:09:42 +0300

    Backend initialization

---
+++ backend/backend/src/test/java/citybike/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package citybike.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
