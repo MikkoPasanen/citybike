85aeab7cbebb1b671ac6b179e19557a1ba13d121
Mikko Pasanen <pasanemikko@gmail.com>
2024-08-30 15:04:06 +0300

    Backend: Cleanup and make stationcontroller use service layer instead of directly using repository

--- backend/backend/src/main/java/citybike/rest/StationsController.java
+++ backend/backend/src/main/java/citybike/rest/StationsController.java
@@ -2,6 +2,7 @@ package citybike.rest;
 
 import citybike.entity.Station;
 import citybike.services.StationRepository;
+import citybike.services.StationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,27 +15,21 @@ import java.util.List;
 @RequestMapping("/stations")
 public class StationsController {
 
-    private final StationRepository stationRepository;
+    private final StationService stationService;
 
-    public StationsController(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
+    public StationsController(StationService stationService) {
+        this.stationService = stationService;
     }
 
     @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/all")
     public ResponseEntity<List<Station>> getAllStations() {
-        List<Station> stations = stationRepository.findAll();
-        return new ResponseEntity<>(stations, HttpStatus.OK);
+        return new ResponseEntity<>(stationService.getAllStations(), HttpStatus.OK);
     }
 
     @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/singular/{id}")
     public ResponseEntity<Station> getStationById(@PathVariable int id) {
-        Station station100 = new Station(100, "Asema sata", "Asemankatu 100", "7.5263", "5.5235");
-        Station station200 = new Station(200, "Asema kaksataa", "Asemankatu 200", "2.1263", "9.2235");
-
-        return id == 100
-                ? new ResponseEntity<>(station100, HttpStatus.OK)
-                : new ResponseEntity<>(station200, HttpStatus.OK);
+        return new ResponseEntity<>(stationService.getStationById(id), HttpStatus.OK);
     }
 }
--- backend/backend/src/main/java/citybike/services/JourneyRepository.java
+++ backend/backend/src/main/java/citybike/services/JourneyRepository.java
@@ -4,11 +4,9 @@ import citybike.entity.Journey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface JourneyRepository extends JpaRepository<Journey, Integer> {
     @Query(value = "SELECT * FROM journey LIMIT :limit", nativeQuery = true)
     List<Journey> findLimited(@Param("limit") int limit);
